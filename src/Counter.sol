e80aab154e1b4358085333eaa0f41b577f23c413
turingtribe <143507004+turingtribe@users.noreply.github.com>
2023-12-15 16:02:54 +0530

    changes in counter.sol

--- src/Counter.sol
+++ src/Counter.sol
@@ -3,6 +3,8 @@ pragma solidity ^0.8.13;
 
 contract Counter {
     uint256 public number;
+    string name;
+    bool etc;
 
     function setNumber(uint256 newNumber) public {
         number = newNumber;
